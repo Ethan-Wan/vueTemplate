ac9433033c2a921fcbdaea1dfa189684323e50a3
wansy <wansy23@163.com>
2017-03-30 16:18:21 +0800

    修改路径问题

--- singlePage/src/main.js
+++ singlePage/src/main.js
@@ -1,9 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './pages/App'
 import router from './router'
+import vueResource from 'vue-resource'
 
+Vue.use(vueResource)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
